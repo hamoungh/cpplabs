3b080a45148cd0eb5b9a3281ff590975bcb5dc4e
hamoun <hamoun.gh@gmail.com>
2014-11-18 07:35:39 -0500

    Update lab_multidimensional_arrays.cpp

--- lab_multidimensional_arrays.cpp
+++ lab_multidimensional_arrays.cpp
@@ -1,1 +1,9 @@
- e
+in this lab we are going to implement the tic-tac-toe game
+
+you need to submit the result at the end of lab to the blackboard
+
+- first define a global three by tree two-dimensional character array to store the state of the game
+each cell in the array can take three values space character, X character,  O character
+
+- create a method called initialize_game that sets all elements of the matrix to space character
+
