020e322060d72df6623a424e9b128d8baed995ac
hamoun <hamoun.gh@gmail.com>
2014-11-18 10:17:19 -0500

    Update lab_multidimensional_arrays.cpp

--- lab_multidimensional_arrays.cpp
+++ lab_multidimensional_arrays.cpp
@@ -5,14 +5,33 @@ you need to submit the result at the end of lab to the blackboard
 - first define a global three by tree two-dimensional character array to store the state of the game
 each cell in the array can take three values space character, X character,  O character
 
-- create a method called initialize_game that sets all elements of the matrix to space character
+- create a method called initialize_game that sets all elements of the matrix to space character. then test it using:
+int main(void)
+{
+  initialize_game();
+}
 
 - create a method called draw() that will draw the game state in the terminal using simple 'cout <<' 
+test it using:
+int main(void)
+{
+  initialize_game();
+  draw();
+}
 
 -  create a method called 'char isThereAWinner()' their returns either O or X or space character (iin case there is no winner). The method should check the three rows (you can do this using it for loop that iterates through the rows), and three columns ( again you can do this using a for loop that a iterates over the columns) ,
 and two diagonals. 
 
-- test 'isThereAWinner'
+- test 'isThereAWinner' using:
+int main(void)
+{
+  char done=  ' ';
+  initialize_game();
+  do {
+    draw();
+  } while(done== ' '); 
+  if(done=='X') printf("You won!\n");
+}
 
 - write a method called 'get_player_move' that is going to read the coordinates of the cell that the user wants to place checkmark. Inside the method check if the specified ccell is empty or not using the below conditions:
   if(matrix[x][y]!= ' '){
@@ -28,10 +47,6 @@ and two diagonals.
 int main(void)
 {
   char done;
-
-  cout<<"This is the game of Tic Tac Toe."<<endl;
-  cout<<"You will be playing against the computer."<<endl;
-
   done =  ' ';
   initialize_game();
 
